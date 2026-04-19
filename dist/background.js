caf5e1f090fff6ec30480cb70f3ce7032cec45ca
xhj <xhj@MacBook-Air.local>
2026-04-20 01:58:03 +0800

    feat: add dist folder for direct download

---
+++ dist/background.js
@@ -0,0 +1,1 @@
+chrome.runtime.onInstalled.addListener(()=>{console.log("Cross-Search extension installed")});chrome.runtime.onStartup.addListener(()=>{console.log("Cross-Search extension started")});
